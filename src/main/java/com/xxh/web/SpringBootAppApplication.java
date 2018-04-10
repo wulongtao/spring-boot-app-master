a1b2e1a618349f2c575493bf01ba6cdd3cc81a46
wulongtao <769418278@qq.com>
2018-04-10 11:11:50 +0800

    Spring Boot项目

---
+++ src/main/java/com/xxh/web/SpringBootAppApplication.java
@@ -0,0 +1,15 @@
+package com.xxh.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+}
